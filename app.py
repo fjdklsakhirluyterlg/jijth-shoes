6eb59cf8946dc0eb8a9fe2c1d838103d697985ca
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-11 08:34:53 +0100

    stuff

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from flask_restful import Resource, Api
 from datetime import datetime
 from werkzeug.utils import secure_filename
 import random
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -211,7 +212,30 @@ def checkout():
     
 @app.route("/upload", methods=["POST", "GET"])
 def upload_stuff():
-    pass     
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))    
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    ''' 
     
 if __name__ == "__main__":
     app.run(port=5040, host="0.0.0.0", debug=True)
