bce28233feee8da54c4d36908a7123021d0ffa38
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-11 17:41:44 +0100

    stuff

--- app.py
+++ app.py
@@ -315,7 +315,7 @@ def upload_stuff():
 
 @app.route("/add")
 def add_stuff():
-    pass
+    return render_template("add.html")
     
 if __name__ == "__main__":
     app.run(port=5040, host="0.0.0.0", debug=True)
