617a748b1e5d40f9e7794b372a6868c70afc5f16
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-11 18:00:17 +0100

    stuff

--- static/add.js
+++ static/add.js
@@ -2,6 +2,7 @@ function myFunction(id){
     var name = document.getElementById("name").value;
     var items = document.getElementById("stock").value;
     var user_id = id;
+    data = {"name": name, "items": items, "user_id": user_id};
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function(){
         console.log("added")
