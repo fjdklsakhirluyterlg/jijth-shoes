cba9ba8e87a7385b6a37fe4387a5816b7784f65c
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-09-11 18:03:41 +0100

    stuff

--- static/add.js
+++ static/add.js
@@ -1,12 +1,14 @@
-function myFunction(id){
-    var name = document.getElementById("name").value;
-    var items = document.getElementById("stock").value;
-    var user_id = id;
-    data = {"name": name, "items": items, "user_id": user_id};
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function(){
-        console.log("added")
+$.ajax({
+    url: '/api/add/category',
+    dataType: 'json',
+    type: 'post',
+    contentType: 'application/json',
+    data: JSON.stringify( { name : $("#name").val(), items : $("#stock").val() } ),
+    processData: false,
+    success: function( data, textStatus, jQxhr ){
+        $('#response pre').html( JSON.stringify( data ) );
+    },
+    error: function( jqXhr, textStatus, errorThrown ){
+        console.log( errorThrown );
     }
-    xhttp.open()
-    xhttp.send();
-}
+});
