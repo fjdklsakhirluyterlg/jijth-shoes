ec87ddf3f948532c98fe88700f3fd9153d439397
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-09 08:44:51 +0100

    stuff

--- static/add.js
+++ static/add.js
@@ -6,10 +6,9 @@ function item(id){
     var price = document.getElementById("price").value;
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function(){
-        console.log("stuff")
-        document.getElementById("name").setAttribute('value', name);
-        document.getElementById("stock").setAttribute('value', stock);
-        document.getElementById("price").setAttribute('value', price);
+        document.getElementById("name").innerHTML = name;
+        document.getElementById("stock").innerHTML = stock;
+        document.getElementById("price").innerHTML = price;
         action = xhttp.responseText
     }
     xhttp.open("GET", `/views/add?name=${name}&stock=${stock}&price=${price}&user_id=${id}`)
