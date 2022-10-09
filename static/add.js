a4265e564fde856c1ab9cfe0c1a45a2ed3a14c76
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-09 09:25:39 +0100

    stuff

--- static/add.js
+++ static/add.js
@@ -17,20 +17,22 @@ function image(){
     const formData = new FormData();
     const photos = document.querySelector('input[type="file"][multiple]');
 
-    for (var i = 0; i < photos.length; i++){
-        formData.append(`file`, photos.files[i])
-    }
-    console.log(photos.files)
-    // fetch(`http://127.0.0.1:5040/api/uploads?id=${action}`, {
-    // method: 'POST',
-    // body: formData,
-    // })
-    // .then(response => response.json())
-    // .then(result => {
-    // console.log('Success:', result);
-    // })
-    // .catch(error => {
-    // console.error('Error:', error);
-    // });
+    formData.append(`file`, photos.files[0])
+
+    // for (var i = 0; i < photos.length; i++){
+    //     formData.append(`file`, photos.files[i])
+    // }
+    // console.log(photos.files)
+    fetch(`http://127.0.0.1:5040/api/uploads?id=${action}`, {
+    method: 'POST',
+    body: formData,
+    })
+    .then(response => response.json())
+    .then(result => {
+    console.log('Success:', result);
+    })
+    .catch(error => {
+    console.error('Error:', error);
+    });
 
 }
