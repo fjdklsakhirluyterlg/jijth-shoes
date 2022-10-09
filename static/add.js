f5a1e5c264f06efa9c054eba93674867ea5a3b34
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2022-10-09 08:40:21 +0100

    stuff

--- static/add.js
+++ static/add.js
@@ -20,7 +20,7 @@ function image(){
     const formData = new FormData();
     const photos = document.querySelector('input[type="file"][multiple]');
 
-    fetch(`http://127.0.0.1:5040/api/uploads?id-${action}`, {
+    fetch(`http://127.0.0.1:5040/api/uploads?id=${action}`, {
     method: 'POST',
     body: formData,
     })
